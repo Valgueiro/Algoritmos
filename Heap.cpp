45cf75bad502ddcc3e998b67acb2c6fd5ef07551
Valgueiro <mvt@cin.ufpe.br>
2016-09-19 22:54:04 -0300

    Revert "Revert "Codigo para criação de heap a partir de um array de entrada""
    This reverts commit 8cdb9b8b802611ad309b0d5c84c0f9a71ea17acd.

---
+++ Heap.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int heap[501];
+int size;
+
+int dad(int i){
+	return (i/2);
+}
+
+int left(int i){
+	return (2*i);
+}
+
+int right(int i){
+	return (2*i+1);
+}
+
+void print(){
+	for(int i=1; i<size; i++){
+		printf(" %d |", heap[i]);
+	}
+	printf(" %d", heap[size]);
+}
+
+void heapify(int i){
+	if(size>=left(i)){
+		if(heap[left(i)]>heap[right(i)]){
+			if(heap[left(i)]>heap[i]){
+				int aux = heap[i];
+				heap[i] = heap[left(i)];
+				heap[left(i)] = aux;
+				heapify(left(i));
+			}
+		}else{
+			if(heap[right(i)]>heap[i]){
+				int aux = heap[i];
+				heap[i] = heap[right(i)];
+				heap[right(i)] = aux;
+				heapify(right(i));
+			}
+		}
+	}
+
+	
+}
+
+int main(){
+	int i;
+	for(i = 1; scanf("%d", &heap[i])==1; i++);
+	i--;
+	size = i;
+	
+	printf("Estado inicial:");
+	print();
+	
+	for(i = (size/2); i>0; i--){
+		heapify(i);
+		printf("\nEstado Atual:");
+		print();
+	}
+	
+	printf("\nResultado Final:");
+	print();
+	return 0;
+}
