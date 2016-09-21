c2319bd537fc686a439439fef139d5b9bb4912d4
Valgueiro <mvt@cin.ufpe.br>
2016-09-21 00:39:55 +0000

    Codigo para Ordenação Merge

---
+++ MergeSort.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void place(int* arr, int start, int middle, int end){
+	int i = start;
+	int j = middle+1;
+	int aux[100];
+	int here = start;
+	while(i<=middle && j<=end){
+		if(arr[i]<arr[j]){
+			aux[here] = arr[i];
+			here++;
+			i++;
+		}else{
+			aux[here] = arr[j];
+			here++;
+			j++;
+		}
+	}
+	while(i<=middle){
+		aux[here] = arr[i];
+		here++;
+		i++;
+	}
+	while(j<=end){
+		aux[here] = arr[j];
+		here++;
+		j++;
+	}
+
+	for(int cont = start; cont<=end; cont++){
+		arr[cont] = aux[cont];
+	}
+}
+
+void merge(int* arr, int start, int end){
+	if(start<end){
+		int middle = (start+end)/2;
+		merge(arr, start, middle);
+		merge(arr, middle+1, end);
+		place(arr, start, middle, end);
+	}
+}
